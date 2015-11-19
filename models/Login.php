dcfed270eb837016da348b771188d5cb84c042a3
logiclogue <jordanpaullord@hotmail.co.uk>
2015-11-19 07:49:21 +0000

    Add ability to login with JSON through get request

--- models/Login.php
+++ models/Login.php
@@ -6,17 +6,22 @@ require dirname(__DIR__) . '/models/Database.php';
 class Login
 {
 	private static $query = 'SELECT hash FROM users WHERE username=:username';
-	private static $username = 'logiclogue';
-	private static $password = 'password';
+	private static $username;
+	private static $password;
 	private static $errorMsg = 'Incorrect username or password';
 
 
 	private static function storeSession() {
 		$_SESSION['username'] = self::$username;
+
+		echo 'Successfully logged in';
 	}
 
 	public static function init() {
 		$result = Database::$conn->prepare(self::$query);
+		$data = json_decode($_GET['data'], true);
+		self::$username = $data['username'];
+		self::$password = $data['password'];
 		
 		// check if query is successful
 		if ($result->execute(array(':username' => self::$username))) {
