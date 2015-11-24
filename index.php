03db70dcb7b0cf278a050bc642dca7199732947c
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-11-24 08:03:45 +0000

    Move most html to seperate files

--- index.php
+++ index.php
@@ -9,32 +9,8 @@
 	<script src="js/Main.js"></script>
 </head>
 <body ng-app="promethius-cms">
-	<div id="page-nav">
-		<ul>
-			<li>Posts</li>
-			<li>Login</li>
-		</ul>
-	</div>
 
-	<div ng-controller="login" id="page-login">
-		<table>
-			<tr>
-				<td>Username</td>
-				<td><input id="input-username" type="text" ng-model="username"></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td><input id="input-password" type="password" ng-model="password"></td>
-			</tr>
-		</table>
+	<div ng-view></div>
 
-		<button ng-click="submit()">Login</button>
-	</div>
-
-	<div id="page-content" ng-controller="content">
-		<h1 id="h1-title" ng-bind="title"></h1>
-		<div id="div-content" ng-bind-html="content"></div>
-		<small id="small-date" ng-bind="date"></small>
-	</div>
 </body>
 </html>
