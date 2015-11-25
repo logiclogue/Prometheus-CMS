6a3d4fa14966a1f36e266eaaa9a66c92625f50ff
logiclogue <jordanpaullord@hotmail.co.uk>
2015-11-25 17:01:13 +0000

    Add basic login script

--- js/Main.js
+++ js/Main.js
@@ -1,15 +1,52 @@
-var app = angular.module('promethius-cms', []);
+var app = angular.module('promethius-cms', ['ngRoute']);
 
 
-app.factory('status', function () {
-	return true;
+app.config(['$routeProvider', function ($routeProvider)
+{
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/content.html',
+		controller: 'ContentCtrl'
+	})
+	.when('/login', {
+		templateUrl: 'views/login.html',
+		controller: 'LoginCtrl'
+	})
+	.otherwise({
+		redirectTo: '/login'
+	});
+}]);
+
+
+app.factory('status', function ($http) {
+	return {
+		get: function (callback) {
+			$http({
+				url: 'models/Status.php',
+				method: 'GET'
+			})
+			.then(function (response) {
+				callback(response.data);
+			});
+		}
+	};
 });
 
 
-app.controller('login', function ($scope, $http, status)
+app.controller('LoginCtrl', function ($scope, $http, $location, status)
 {
+	var data = {};
+
+
+	function checkLoggedIn(response) {
+		if (JSON.parse(response)) {
+			$location.path('/');
+		}
+	}
+
+
 	$scope.submit = function () {
-		var data = {
+		data = {
 			command: 'login',
 			username: $scope.username,
 			password: $scope.password
@@ -20,34 +57,53 @@ app.controller('login', function ($scope, $http, status)
 			method: 'POST',
 			data: 'JSON=' + JSON.stringify(data),
 			headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-		}).then(function (response) {
-			console.log(response.data);
+		})
+		.then(function (response) {
+			checkLoggedIn(response.data);
 		});
 	};
+
+	status.get(function (response) {
+		checkLoggedIn(response.logged_in);
+	});
 });
 
 
-app.controller('content', function ($scope, $sce, $http)
+app.controller('ContentCtrl', function ($scope, $sce, $http, status)
 {
 	var data = {
 		title: 'Test Title'
 	};
+
+	$scope.isLoggedIn = false;
 
 	$http({
 		url: 'models/GetPost.php',
 		method: 'POST',
 		data: 'JSON=' + JSON.stringify(data),
 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-	}).then(function (response) {
+	})
+	.then(function (response) {
 		$scope.title = response.data.title;
 		$scope.content = $sce.trustAsHtml(response.data.content);
 		$scope.date = response.data.date;
 	});
 
-	$http({
-		url: 'models/Status.php',
-		method: 'GET'
-	}).then(function (response) {
-		console.log(response.data);
+	status.get(function (response) {
+		$scope.isLoggedIn = JSON.parse(response.logged_in);
 	});
+
+
+	$scope.logout = function () {
+		var data = {
+			command: 'logout'
+		}
+
+		$http({
+			url: 'models/Login.php',
+			method: 'POST',
+			data: 'JSON=' + JSON.stringify(data),
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+		});
+	};
 });
