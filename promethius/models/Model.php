4fdd9f1e0f719d05d385bb413f60ae015d83e9fe
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-21 17:23:06 +0000

    Comment Model class

--- promethius/models/Model.php
+++ promethius/models/Model.php
@@ -3,17 +3,42 @@
 require_once(dirname(__DIR__) . '/functions/GetJSON.php');
 
 
+/**
+ * Model class, the class is extended by all models. It provides a foundation for all models.
+ *
+ * @class Model
+ */
+
 class Model
 {
+	/**
+	 * The data object is used to store.
+	 *
+	 * @property
+	 * @type {Object}
+	 */
 	protected static $data = array();
 
 
+	/**
+	 * Call allows PHP to pass data into the model.
+	 *
+	 * @method
+	 * @param {Object} Data object to interact with the model
+	 * @return {}
+	 */
 	public static function call($data) {
 		self::$data = $data;
 
 		return static::main();
 	}
 
+
+	/**
+	 * Function that is called to check if it is called with Post.
+	 *
+	 * @method
+	 */
 	public static function init() {
 		if (GetJSON::isPost()) {
 			self::$data = GetJSON::decodePost();
