2a8ac956b3eb8a1bf872c0a5c8dbd689fd702ec3
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-28 12:33:48 +0000

    Comment Status method

--- models/Status.php
+++ models/Status.php
@@ -3,17 +3,40 @@
 session_start();
 
 
+/**
+ * Model for getting the status of the user.
+ *
+ * @class Status
+ */
 class Status
 {
+	/**
+	 * Array for storing the user data to be returned.
+	 *
+	 * @property all
+	 * @type Array
+	 */
 	private static $all = array();
 
 
+	/**
+	 * Method for encoding the data as JSON.
+	 * Also echos that encoded data.
+	 *
+	 * @method encode
+	 */
 	private static function encode() {
 		$json = json_encode(self::$all);
 
 		echo $json;
 	}
 
+	/**
+	 * Fetches user data if logged in.
+	 * Then calls @method encode.
+	 *
+	 * @method init.
+	 */
 	public static function init() {
 		if (isset($_SESSION['user'])) {
 			self::$all['user'] = $_SESSION['user'];
