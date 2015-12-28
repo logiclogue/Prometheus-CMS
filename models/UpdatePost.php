0fd7a3ea645989de31f5c8f537934b61a5fe74d5
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-28 12:45:37 +0000

    Comment UpdatePost model

--- models/UpdatePost.php
+++ models/UpdatePost.php
@@ -5,14 +5,34 @@ require_once(dirname(__DIR__) . '/models/Post.php');
 session_start();
 
 
+/**
+ * Model for updating post's content, title, etc.
+ *
+ * @class UpdatePost.
+ * @extends Post
+ * @static
+ */
 class UpdatePost extends Post
 {
+	/**
+	 * SQL query string for updating a post.
+	 * Updates title and content.
+	 *
+	 * @property query
+	 * @type String
+	 */
 	private static $query = <<<SQL
 		UPDATE posts
 		SET title=:title, content=:content
 		WHERE id=:id
 SQL;
 
+	/**
+	 * SQL query string for deleting the links to tags.
+	 *
+	 * @property query_delete_tags
+	 * @type String
+	 */
 	private static $query_delete_tags = <<<SQL
 		DELETE post_tag_maps.*
 		FROM post_tag_maps
@@ -22,6 +42,12 @@ SQL;
 SQL;
 
 
+	/**
+	 * Method for deleting all tags associated with the post.
+	 *
+	 * @method deleteTags
+	 * @return {Boolean} Whether tags creation and deletion was successful.
+	 */
 	private static function deleteTags() {
 		$stmt_delete_tags = Database::$conn->prepare(self::$query_delete_tags);
 
@@ -35,11 +61,23 @@ SQL;
 		}
 	}
 
+	/**
+	 * Method for binding parameters to @property stmt
+	 *
+	 * @method bindParams
+	 */
 	private static function bindParams() {
 		self::bindTitleContent();
 		self::$stmt->bindParam(':id', self::$data['id']);
 	}
 
+	/**
+	 * Method for preparing @property stmt.
+	 * Also executes it.
+	 *
+	 * @method update
+	 * @return {Boolean} Whether successfully updated post.
+	 */
 	private static function update() {
 		self::$stmt = Database::$conn->prepare(self::$query);
 
@@ -53,6 +91,13 @@ SQL;
 		}
 	}
 
+	/**
+	 * Checks whether user is logged in.
+	 * Then calls @method update.
+	 *
+	 * @method main
+	 * @return {Boolean} Whether successfully updated post and logged in.
+	 */
 	protected static function main() {
 		// check to see if user is logged in before updating post
 		if (isset($_SESSION['user']['id'])) {
